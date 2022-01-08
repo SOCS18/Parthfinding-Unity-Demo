876393cf0ca88a731225044eedb67e609252c6b6
Saul Carreon <sacarreon18@gmail.com>
2022-01-07 21:36:43 -0600

    Added demo to repository

---
+++ 08-Navigation/Assets/MoveObstacle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObstacle : MonoBehaviour
+{
+    [SerializeField] private float mag = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * mag * Time.deltaTime;
+    }
+}
